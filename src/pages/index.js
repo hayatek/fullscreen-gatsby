82fcadc3dda7c6024d4ebcb15d8ae7f3d67d83c0
hayatek <stream.hyt@gmail.com>
2022-04-24 23:45:49 +0900

    update css file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { header, btn } from "../styles/home.module.css"
+import * as homeStyles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section className={header}>
+      <section className={homeStyles.content}>
         <div>
-          {/* <video muted loop autoplay>
-            <source src="/stars.mp4" type="video/mp4" />
-          </video> */}
           <video autoPlay loop muted playsInline>
             <source src="/sea.mp4" />
           </video>
-          <div className="overlay"></div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>web developer</p>
-          <Link className={btn} to="/work">
-            My Portfolio Projects
-          </Link>
+          <div className={homeStyles.overlay}></div>
+          <div className={homeStyles.text}>
+            <h2>Hello World!</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+              dolor aperiam expedita rem impedit perspiciatis exercitationem qui
+              vel, possimus neque adipisci obcaecati rerum in alias, sunt
+              aspernatur at harum cupiditate.
+            </p>
+            <Link className={homeStyles.btn} to="/work">
+              My Portfolio Projects
+            </Link>
+          </div>
         </div>
         {/* <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} /> */}
       </section>
